67b7d7ee287a0ddbc8376315307821ed15f8cca7
rydens <rdensmore365@gmail.com>
2023-12-07 04:44:21 -0500

    implement validBoard

---
+++ SudokuSolver.hs
@@ -0,0 +1,28 @@
+module SudokuSolver where
+
+import System.Random
+
+import SudokuBoard
+
+hasNoDups :: [Cell] -> Bool
+hasNoDups [] = True
+hasNoDups [x] = True
+hasNoDups (Empty:xs) = True && (hasNoDups xs)
+hasNoDups (x:y:xs) = (x /= y) && hasNoDups (x:xs) && hasNoDups (y:xs)
+
+
+-- validates a single Square
+validSquare :: Square -> Bool
+validSquare s = let cells = foldr (\x y -> thrupleElems x ++ y) [] (thrupleElems s)
+                in  hasNoDups cells
+
+-- Checks if board is valid, ie no repeating numbers in each square, row, and column.
+-- Empty cells are ok.
+validBoard :: Board -> Bool
+validBoard board = let squares = getSquares board
+                       rows = getRows board
+                       columns = getColumns board
+                   in  and $ (map validSquare squares) ++ (map hasNoDups rows) ++ (map hasNoDups columns)
+
+
+-- generate a solved board
