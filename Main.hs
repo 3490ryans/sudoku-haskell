208c7a10296bf605aef2cc3417aa7e31fc806438
rydens <rdensmore365@gmail.com>
2023-12-08 10:28:17 -0500

    finish UI

--- Main.hs
+++ Main.hs
@@ -14,13 +14,20 @@ import SudokuSolver (validBoard, solveBoard)
 generateCoords :: String -> [Coord]
 generateCoords s = take (length s) [(y,x) | y <- ['a'..'i'], x <- [1..9]]
 
-parseCell :: Char -> Cell
-parseCell c | c `elem` ['1'..'9'] = Num (toInteger $ digitToInt c)
-            | c == '0'            = Empty
+parseCell :: Char -> String -> Cell
+parseCell c v | c `elem` ['1'..'9'] = Num (toInteger $ digitToInt c)
+              | c `elem` v = Empty
 
-parseBoard :: String -> Board
-parseBoard s = bulkUpdateBoard emptyBoard updates 
-               where updates = zip (generateCoords s) (foldr (\c cs -> (parseCell c) : cs) [] s)
+cleanString :: String -> String -> String
+cleanString [] _ = []
+cleanString (x:xs) v | x `elem` ['1'..'9'] = x : cleanString xs v
+                     | x `elem` v = x : cleanString xs v
+                     | otherwise  = cleanString xs v
+
+parseBoard :: String -> String -> Board
+parseBoard s v = let i = cleanString s v
+                     updates = zip (generateCoords i) (foldr (\c cs -> (parseCell c v) : cs) [] i)
+                 in bulkUpdateBoard emptyBoard updates
 
 
 -- Printing
@@ -29,36 +36,36 @@ intraBorder  = "╟───┼───┼───╫───┼───┼�
 midBorder    = "╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣\n"
 bottomBorder = "╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝\n"
 
-printCellPretty :: Cell -> String
-printCellPretty (Num n) = show n
-printCellPretty (Empty) = " "
+printCellPretty :: Cell -> Char -> String
+printCellPretty (Num n) _ = show n
+printCellPretty (Empty) v = [v]
 
-printCellsPretty :: (Cell,Cell,Cell) -> String
-printCellsPretty (c1,c2,c3) = (printCellPretty c1) ++ " │ " ++ (printCellPretty c2) ++ " │ " ++ (printCellPretty c3)
+printCellsPretty :: (Cell,Cell,Cell) -> Char -> String
+printCellsPretty (c1,c2,c3) v = (printCellPretty c1 v) ++ " │ " ++ (printCellPretty c2 v) ++ " │ " ++ (printCellPretty c3 v)
 
-printSquareRowPretty :: Square -> Char -> String
-printSquareRowPretty (cs1,_,_) 'a' = printCellsPretty cs1
-printSquareRowPretty (_,cs2,_) 'b' = printCellsPretty cs2
-printSquareRowPretty (_,_,cs3) 'c' = printCellsPretty cs3
+printSquareRowPretty :: Square -> Char -> Char -> String
+printSquareRowPretty (cs1,_,_) 'a' v = printCellsPretty cs1 v
+printSquareRowPretty (_,cs2,_) 'b' v = printCellsPretty cs2 v
+printSquareRowPretty (_,_,cs3) 'c' v = printCellsPretty cs3 v
 
-printRowPretty :: (Square,Square,Square) -> Char -> String
-printRowPretty (s1,s2,s3) c = "║ " ++ (printSquareRowPretty s1 c) ++ " ║ " ++ (printSquareRowPretty s2 c) ++ " ║ " ++ (printSquareRowPretty s3 c) ++ " ║\n"
+printRowPretty :: (Square,Square,Square) -> Char -> Char -> String
+printRowPretty (s1,s2,s3) c v = "║ " ++ (printSquareRowPretty s1 c v) ++ " ║ " ++ (printSquareRowPretty s2 c v) ++ " ║ " ++ (printSquareRowPretty s3 c v) ++ " ║\n"
 
-printSquaresPretty :: (Square,Square,Square) -> String
-printSquaresPretty s = (printRowPretty s 'a') ++
-                       intraBorder ++
-                       (printRowPretty s 'b') ++
-                       intraBorder ++
-                       (printRowPretty s 'c')
+printSquaresPretty :: (Square,Square,Square) -> Char -> String
+printSquaresPretty s v = (printRowPretty s 'a' v) ++
+                         intraBorder ++
+                         (printRowPretty s 'b' v) ++
+                         intraBorder ++
+                         (printRowPretty s 'c' v)
 
-printBoardPretty :: Board -> String
-printBoardPretty (ss1,ss2,ss3) = topBorder ++
-                                 (printSquaresPretty ss1) ++
-                                 midBorder ++
-                                 (printSquaresPretty ss2) ++
-                                 midBorder ++
-                                 (printSquaresPretty ss3) ++
-                                 bottomBorder
+printBoardPretty :: Board -> Char -> String
+printBoardPretty (ss1,ss2,ss3) v = topBorder ++
+                                   (printSquaresPretty ss1 v) ++
+                                   midBorder ++
+                                   (printSquaresPretty ss2 v) ++
+                                   midBorder ++
+                                   (printSquaresPretty ss3 v) ++
+                                   bottomBorder
 
 -- saveBoardToFile :: String -> FilePath -> IO ()
 saveBoardToFile :: String -> FilePath -> IO ()
@@ -72,138 +79,91 @@ saveBoardToFile boardString filePath = do
 readBoardFromFile :: FilePath -> IO Board
 readBoardFromFile filePath = do
     content <- readFile filePath
-    return (parseBoard content)
-
-boardToString :: Board -> String
-boardToString (rows1, rows2, rows3) =
-    concatMap getRows [rows1, rows2, rows3]
-  where
-    getRows :: (Square, Square, Square) -> String
-    getRows (s1, s2, s3) =
-        concatMap getRow ['a', 'b', 'c']
-      where
-        getRow :: Char -> String
-        getRow y =
-            printSquareRowPretty s1 y ++ " " ++
-            printSquareRowPretty s2 y ++ " " ++
-            printSquareRowPretty s3 y ++ "\n"
+    return (parseBoard content ".0")
 
 
-exampleBoard = "000000013400200000600000000000460500010000007200500000000031000000000420080000000"
+
+clearScreen :: IO ()
+clearScreen = do
+    putStr "\ESC[2J"
+
+playSudoku :: Board -> String -> IO ()
+playSudoku board status = do
+    clearScreen
+    putStrLn "Add a number to a cell by addressing it via its coordinate, ie 'a4 5'"
+    putStrLn "Type 'check' to check your solution, 'save' to save the board to a file for later, and 'solve' to solve the board!\n"
+    putStrLn $ status ++ "\n"
+    putStr $ printBoardPretty board ' '
+    putStr "\n> "
+    input <- getLine
+    case input of
+        "q" -> return ()
+        "Q" -> return ()
+        "check" -> if validBoard board
+                   then playSudoku board "Looks good!"
+                   else playSudoku board "Not quite!"
+        "save" -> do
+            putStrLn "Enter the path to save your Sudoku board file (.sudoku):"
+            putStr "> "
+            savePath <- getLine
+            let boardString = printBoardPretty board '.'
+            -- Save the Sudoku board string to the specified file
+            saveBoardToFile boardString savePath
+            putStrLn $ "Sudoku board saved to file: " ++ savePath
+        "solve" -> do
+            solvedBoard <- solveBoard board
+            playSudoku solvedBoard "Solved!"
+        (y:x:space:n:[]) | y `elem` ['a'..'i'] && x `elem` ['1'..'9'] && isSpace space && n `elem` ['1'..'9'] -> playSudoku (updateBoard board (y,(fromIntegral $ digitToInt x)) (Num (fromIntegral $ digitToInt n))) ""
+                         | otherwise -> playSudoku board ""
+        t -> playSudoku board ""
+
+genSudokuMenu :: IO ()
+genSudokuMenu = do
+    clearScreen
+    putStrLn "Let's play! Enter a difficulty, or number of missing cells."
+    putStr "> "
+    input <- getLine
+    case input of
+        "q" -> return ()
+        "Q" -> return ()
+        t | and $ map isDigit t -> let n = (read input :: Integer)
+                                  in if n >= 0 && n <= 81
+                                     then do
+                                        board <- generatePuzzle n
+                                        playSudoku board ""
+                                     else genSudokuMenu
+          | otherwise -> genSudokuMenu
+
+loadSudokuMenu :: IO ()
+loadSudokuMenu = do
+    putStrLn "Enter the path to your Sudoku board file (.sudoku):"
+    putStr "> "
+    filePath <- getLine
+
+    result <- try (readBoardFromFile filePath) :: IO (Either SomeException Board)
+    case result of
+        Left e -> putStrLn $ "Error reading the Sudoku board: " ++ show e
+        Right newBoard -> do
+            playSudoku newBoard "Loaded!"
+
+introPrompt :: IO ()
+introPrompt = do
+    clearScreen
+    putStrLn "Welcome to Sudoku! Press Q at any time to Quit."
+    putStrLn "1. Generate a new Sudoku Puzzle"
+    putStrLn "2. Load a .sudoku File"
+    putStr "> "
+    input <- getLine
+    case input of
+        "q" -> return ()
+        "Q" -> return ()
+        "1" -> genSudokuMenu
+        "2" -> loadSudokuMenu
+        otherwise -> introPrompt
+
+
 
 main :: IO ()
 main = do
-    putStrLn "Sudoku Menu:"
-    putStrLn "1. Generate a new Sudoku Puzzle"
-    putStrLn "2. Load a .sudoku File"
-    putStrLn "Q. Quit"
-    putStr "Choose an option: "
-    choice <- getLine
-    handleChoice choice emptyBoard  -- Pass emptyBoard as the initial board
-
-handleChoice :: String -> Board -> IO ()
-handleChoice "1" currentBoard = do
-    -- Put instructions to generate sudoku Puzzle here
-    putStrLn "Generating a new Sudoku Puzzle..."
-    newBoard <- generatePuzzle 5
-    putStrLn $ printBoardPretty newBoard
-    subMenu newBoard
-
-handleChoice "2" currentBoard = do
-    -- Put instructions on how to load .sudoku file here
-    putStrLn "Enter the path to your Sudoku board file (.sudoku):"
-    filePath <- getLine
-
-    result <- try (readBoardFromFile filePath) :: IO (Either SomeException Board)
-    case result of
-        Left e -> putStrLn $ "Error reading the Sudoku board: " ++ show e
-        Right newBoard -> do
-            putStrLn $ printBoardPretty newBoard
-            putStrLn $ "Sudoku board loaded from file: " ++ filePath
-            subMenu newBoard
-
-handleChoice "Q" _ = do
-    putStrLn "Quitting..."
-    exitSuccess
-
-handleChoice _ currentBoard = do
-    putStrLn "Invalid choice. Please choose again."
-    main
-
-subMenu :: Board -> IO ()
-subMenu currentBoard = do
-    putStrLn "Submenu:"
-    putStrLn "1. Generate a new Sudoku Puzzle"
-    putStrLn "2. Load a .sudoku File"
-    putStrLn "3. Solve Sudoku Puzzle"
-    putStrLn "4. Check Sudoku Puzzle"
-    putStrLn "5. Save Sudoku Puzzle"
-    putStrLn "Q. Quit"
-    putStr "Choose an option: "
-    choice <- getLine
-    handleSubChoice choice currentBoard
-
-handleSubChoice :: String -> Board -> IO ()
-handleSubChoice "1" currentBoard = do
-    -- Put instructions to generate sudoku Puzzle here
-    putStrLn "Generating a new Sudoku Puzzle..."
-    newBoard <- generatePuzzle 5
-    putStrLn $ printBoardPretty newBoard
-    subMenu newBoard
-
-handleSubChoice "2" currentBoard = do
-    -- Put instructions on how to load .sudoku file here
-    putStrLn "Enter the path to your Sudoku board file (.sudoku):"
-    filePath <- getLine
-
-    result <- try (readBoardFromFile filePath) :: IO (Either SomeException Board)
-    case result of
-        Left e -> putStrLn $ "Error reading the Sudoku board: " ++ show e
-        Right newBoard -> do
-            putStrLn $ printBoardPretty newBoard
-            putStrLn $ "Sudoku board loaded from file: " ++ filePath
-            subMenu newBoard
-
-handleSubChoice "3" currentBoard = do
-    -- Put instructions to SOLVE sudoku Puzzle here
-    solvedBoard <- solveBoard currentBoard
-    putStrLn "Solved Sudoku Puzzle:"
-    putStrLn $ printBoardPretty solvedBoard
-    subMenu solvedBoard
-
-handleSubChoice "4" currentBoard = do
-    -- Put instructions to CHECK sudoku Puzzle here
-    let isValid = validBoard currentBoard  
-    subMenu currentBoard
-
-handleSubChoice "5" currentBoard = do
-    -- Put instructions to SAVE sudoku Puzzle here
-    -- Saving Process
-    putStrLn "Do you want to save the Sudoku board? Saving will stop the program!! (y/n)"
-    userChoice <- getLine
-
-    case userChoice of
-        "y" -> do
-            putStrLn "Enter the path to save your Sudoku board file (.sudoku):"
-            savePath <- getLine
-
-            let boardString = boardToString currentBoard
-
-            -- Save the Sudoku board string to the specified file
-            saveBoardToFile boardString savePath
-
-            putStrLn $ "Sudoku board saved to file: " ++ savePath
-
-        "n" -> putStrLn "Sudoku board not saved."
-
-        _ -> do
-            putStrLn "Invalid choice. Please enter 'y' or 'n'."
-            subMenu currentBoard
-
-handleSubChoice "Q" _ = do
-    putStrLn "Quitting..."
-    exitSuccess
-
-handleSubChoice _ currentBoard = do
-    putStrLn "Invalid choice. Please choose again."
-    subMenu currentBoard
+    clearScreen
+    introPrompt
